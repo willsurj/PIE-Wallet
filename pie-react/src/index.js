898afa7b27b0bb4fe0815fc5cfcc1c01cf817b78
Will <willsurj0@gmail.com>
2021-07-15 11:30:55 +1000

    Test implementation of Chart.js and React

--- pie-react/src/index.js
+++ pie-react/src/index.js
@@ -1,10 +1,57 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import ReactDOM from "react-dom";
+import { Pie } from "react-chartjs-2";
 import "./index.css";
+
+const Chart = () => {
+    const [chartData, setChartData] = useState({});
+
+    const circle = () => {
+        setChartData({
+            labels: ["sample1", "sample2", "sample3", "sample4"],
+            datasets: [
+                {
+                    label: "$USD",
+                    data: [45, 79, 23, 165],
+                    backgroundColor: [
+                        "#00ff80",
+                        "#00ffff",
+                        "#007fff",
+                        "#0000ff",
+                        "#8000ff",
+                        "#ff00ff",
+                        "#ff0080",
+                        "#ff0000",
+                        "#ff8000",
+                        "#ffff00",
+                        "#80ff00",
+                        "#00ff00",
+                    ],
+                    borderWidth: 4,
+                },
+            ],
+        });
+    };
+
+    useEffect(() => {
+        circle();
+    }, []);
+
+    return (
+        <div className="chart">
+            <Pie data={chartData} />
+        </div>
+    );
+};
 
 class App extends React.Component {
     render() {
-        return <h1>PIE</h1>;
+        return (
+            <div>
+                <h1>PIE</h1>
+                <Chart />
+            </div>
+        );
     }
 }
 
